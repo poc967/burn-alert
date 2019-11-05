821730ea9499819b03d8075c8c3b2bd51a325735
Patrick O'Connor <poc.967@gmail.com>
2019-11-04 20:39:00 -0500

    completed first sunscreen intent without date specificity

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const Alexa = require('ask-sdk-core')
+const axios = require('axios')
+
+function getUvIndex(location) {
+    const apikey = process.env.DarkSky_API_KEY
+
+    return axios.get(`https://api.darksky.net/forecast/${apikey}/${location}?exclude=minutely,hourly,daily`).then((response) => {
+        return response.data.currently.uvIndex
+    })
+}
+
 const sunscreenIntentHandler = {
     canHandle(handlerInput) {
         return handlerInput.requestEnvelope.request.type === 'IntentRequest'
@@ -6,14 +17,17 @@ const sunscreenIntentHandler = {
     handle(handlerInput) {
         const uvDate = handler.Input.requestEnvelope.reuest.intent.slots.Date.value
         const uvLocation = handler.Input.requestEnvelope.reuest.intent.slots.Location.value
-        const response = `The requested Date is ${uvDate} and the location is ${uvLocation}`
         const reprompt = 'Would you like more information?'
 
-        return handlerInput.responseBuilder
-            .speak(response + reprompt)
-            .repromt(reprompt)
-            .withShouldEndSession(false)
-            .getResponse();
+        return getUvIndex(uvDate, uvLocation).then((uvIndex) => {
+            const speechText = `The current UV Index is ${uvIndex}`
+
+            return handlerInput.esponseBuilder
+                .speak(speechText)
+                .reprompt(reprompt)
+                .withShouldEndSession(false)
+                .getResponse();
+        })
     }
 }
 
